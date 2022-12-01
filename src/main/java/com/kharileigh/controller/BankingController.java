9479d08231da19771a0555fb4f78bde4c8b13f5f
kharileigh <kharileighmiller@gmail.com>
2022-12-01 21:57:52 +0000

    SearchByAccountId Created - error

--- src/main/java/com/kharileigh/controller/BankingController.java
+++ src/main/java/com/kharileigh/controller/BankingController.java
@@ -5,10 +5,12 @@
 
 package com.kharileigh.controller;
 
+import com.kharileigh.entity.Customer;
 import com.kharileigh.model.service.BankingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -27,6 +29,30 @@ public class BankingController {
         return new ModelAndView("index");
     }
     
-    //==================================================
+    //====================== SEARCH ACCOUNT BY ID ============================
+    @RequestMapping("/searchByAccountIdPage")
+    public ModelAndView searchByAccountIdController() {
     
+        return new ModelAndView("SearchAccountById");
+    }
+    
+    @RequestMapping("/searchByAccountId")
+    public ModelAndView searchByAccountId(@RequestParam("customerId") int id) {
+        
+        ModelAndView modelAndView = new ModelAndView();
+        
+        Customer customer = service.searchCustomerById(id);
+        if(customer != null) {
+        
+            modelAndView.addObject("customer", customer);
+            modelAndView.setViewName("ShowAccount");
+        
+        } else {
+        
+            modelAndView.addObject("message", "Account ID : " + id + "does not exist in our system.");
+            modelAndView.setViewName("Output");
+        }
+        
+        return modelAndView;
+    }
 }
