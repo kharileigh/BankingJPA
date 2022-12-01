8ca716012b96ee805c31f5c985884d74757d4c53
kharileigh <kharileighmiller@gmail.com>
2022-12-01 17:24:42 +0000

    Initialised Spring Boot Project

---
+++ src/main/java/com/kharileigh/client/BankingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kharileigh.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingJpaApplication.class, args);
+	}
+
+}
