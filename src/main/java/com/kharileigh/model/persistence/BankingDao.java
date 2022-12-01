dc4a64900b96019f0eb92a764e9185791f9e9c97
kharileigh <kharileighmiller@gmail.com>
2022-12-01 20:53:15 +0000

    Customer DAO created

---
+++ src/main/java/com/kharileigh/model/persistence/BankingDao.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author kharileigh
+ */
+
+package com.kharileigh.persistence;
+
+import com.kharileigh.entity.Customer;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository("dao")
+public interface BankingDao extends JpaRepository<Customer, Integer> {
+    
+    @Query("FROM Customer WHERE ACCOUNT_ID = :id")
+    List<Customer> searchByAccountId(@Param("id") int id);
+    
+    @Query("UPDATE Customer SET BALANCE = BALANCE + :update WHERE ACCOUNT_ID = :id")
+    int updateBalance(@Param("id") int id, @Param("update") double deposit);
+}
